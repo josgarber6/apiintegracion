1948cfb13276891a3f4f046b6dd6e7cbd43a31a4
josgarber6 <josgarber6@alum.us.es>
2022-05-26 23:17:46 +0200

    ProductResourceTest casi fino

--- src/test/java/aiss/api/resources/ProductResourceTest.java
+++ src/test/java/aiss/api/resources/ProductResourceTest.java
@@ -1,15 +1,16 @@
 package aiss.api.resources;
 
 import aiss.model.consuming.Product;
-import aiss.model.repository.MapMarketRepository;
+//import aiss.model.repository.MapMarketRepository;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.util.Collection;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.restlet.resource.ResourceException;
 
 import aiss.api.consuming.resources.ProductResource;
 
@@ -22,12 +23,12 @@ public class ProductResourceTest {
 	public static void setup() throws Exception {
 		
 		// Test product 1
-		product1 = pr.addProduct(new Product("DDJ-1000", "5", "0","100","DJ Controllers","1299,90"));
+		product1 = pr.addProduct(new Product("DDJ-1000", "2.0", "0","100","DJ Controllers","1299.90"));
 		
-//		product3 = pr.addProduct(MapMarketRepository.productMap.get(pr))
+//		product3 = pr.addProduct(MapMarketRepository.productMap.get("p5"));
 		
-		// Test song 2
-		product2 = pr.addProduct(new Product("DDJ-400", "4.5", "40","1000","DJ Controllers","289,90"));
+		// Test product 2
+		product2 = pr.addProduct(new Product("DDJ-400", "4.5", "40","1000","DJ Controllers","289.90"));
 		
 	}
 	
@@ -41,7 +42,7 @@ public class ProductResourceTest {
 	public void testGetAll() {
 		Collection<Product> products = pr.getAll();
 		
-		assertNotNull("The collection of songs is null", products);
+		assertNotNull("The collection of products is null", products);
 		
 		// Show result
 		System.out.println("Listing all products:");
@@ -49,6 +50,82 @@ public class ProductResourceTest {
 		for (Product p : products) {
 			System.out.println("Product " + i++ + " : " + p.getName() + " (ID=" + p.getId() + ")");
 		}
+	}
+	
+	@Test
+	public void testGetProduct() {
+		Product product = pr.getProduct(product2.getId());
+		assertEquals("The id of the products do not match", product2.getId(), product.getId());
+
+		// Show result
+		System.out.println("Product name: " +  product.getId());
+		System.out.println("Product average popularity: " +  product.getAveragePopularity());
+	}
+	
+	@Test
+	public void testAddProduct() {
+		
+		String productName = "Patatas";
+		String averagePopularity = "3";
+		String availability = "5";
+		String totalStock = "10";
+		String averagePrice = "1.5";
+		String category = "FOOD";
+		
+		product4 = pr.addProduct(new Product(productName, averagePopularity, availability, totalStock, category, averagePrice));
+		
+		assertNotNull("Error when adding the product", product4);
+		assertEquals("The product's name has not been setted correctly", productName, product4.getName());
+		assertEquals("The product's category has not been setted correctly", category, product4.getCategory());
+
+	}
+	
+	@Test
+	public void testUpdateProduct() {
+		
+		String productName = "Zanahoria";
+		String averagePopularity = "4.0";
+		String availability = "5";
+		String totalStock = "20";
+		String averagePrice = "1.5";
+		String category = "FOOD";
+		
+		
+		// Update product
+		product2.setName(productName);
+		product2.setAveragePopularity(Double.valueOf(averagePopularity));
+		product2.setAvailability(Integer.valueOf(availability));
+		product2.setTotalStock(Integer.valueOf(totalStock));
+		product2.setAveragePrice(Double.valueOf(averagePrice));
+		product2.setCategory(category);
+		
+		boolean success = pr.updateProduct(product2);
+		
+		assertTrue("Error when updating the product", success);
+		
+		Product product  = pr.getProduct(product2.getId());
+		
+		if (success) {
+			System.out.println("Canción actualizada correctamente");
+		}
+		assertEquals("The product's name has not been updated correctly", productName, product.getName());
+		assertEquals("The product's average popularity has not been updated correctly", averagePopularity, product.getAveragePopularity());
+		assertEquals("The product's availability has not been updated correctly", availability, product.getAvailability());
+		assertEquals("The product's total stock has not been updated correctly", totalStock, product.getTotalStock());
+		assertEquals("The product's total average price has not been updated correctly", averagePrice, product.getAveragePrice());
+		assertEquals("The product's total category has not been updated correctly", category, product.getCategory());
+	}
+	
+	@Test(expected = ResourceException.class)
+	public void testDeleteProduct() {
+		
+		// Delete products
+		boolean success = pr.deleteProduct(product4.getId());
+		
+		assertTrue("Error when deleting the product", success);
+		
+		Product product = pr.getProduct(product4.getId());
+		assertNull("The product has not been deleted correctly", product);
 	}
 	
 }
